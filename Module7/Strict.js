9f979da6e90ea7cad7f4e3dbe569ea53389651e3
Arman Salmani <armansalmani596@gmail.com>
2024-02-13 23:48:43 +0530

    Strict and Unstrict

---
+++ Module7/Strict.js
@@ -0,0 +1,12 @@
+// Strict mode :  It enforces stricter parsing and error handling on the code at runtime. 
+ 
+// Without using strict mode 
+// a ='Arman'
+// console.log(a)
+
+`use strict`
+// a= 'Arman'
+// console.log(a) // here it will give error because we didn't define any key to variable a 
+
+let a ='Arman' 
+console.log(a)
