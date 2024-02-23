df5495234d1eae45dd2326be8214208a2ee47582
Arman Salmani <armansalmani596@gmail.com>
2024-02-23 20:59:37 +0530

    Call method in JS

---
+++ Module7/Callmethod.js
@@ -0,0 +1,49 @@
+/* The call() method is a predefined JavaScript method.
+It can be used to invoke (call) a method with an owner object  as an argument (parameter).*/
+
+// Before Using Call method
+
+let Person1={
+    name:'Arman',
+    lname: 'Salmani',
+    Age:20,
+
+    CallingName:function(){
+        console.log(`Hii I'm ${this.name} ${this.lname} and my Age is ${this.Age} `)
+    }
+}
+
+let Person2={
+    name: 'Ninja',
+    lname: 'Hattori',
+    Age: 45120,
+
+    CallingName:function(){
+        console.log(`Hii I'm ${this.name} ${this.lname} and my Age is ${this.Age} `)
+    }
+}
+Person1.CallingName()
+Person2.CallingName()
+
+// After using Call method 
+
+let  Person3={
+    name:'Arman',
+    lname: 'Salmani',
+    Age:20,
+
+    CallingName:function(){
+        console.log(`Hii I'm ${this.name} ${this.lname} and my Age is ${this.Age} `)
+    }
+}
+
+ let Person4={
+    name: 'Ninja',
+    lname: 'Hattori',
+    Age: 45120,
+
+    CallingName:function(){
+        console.log(`Hii I'm ${this.name} ${this.lname} and my Age is ${this.Age} `)
+    }
+}
+Person4.CallingName.call(Person4) // Here we are invoking CallingName method for Person2 
