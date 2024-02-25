52c625170b7973d44fbdf0a3acddbd5c3bda5911
Arman Salmani <armansalmani596@gmail.com>
2024-02-25 23:50:03 +0530

    SetInterval Function in JS

---
+++ Module8/SetInterval.js
@@ -0,0 +1,10 @@
+function Hello(){
+    console.log("Hello Arman")
+}
+
+setInterval(Hello,1000) 
+
+//setInterval() is a function in JavaScript that is used to repeatedly execute 
+//a given function at a specified interval. 
+
+
---
+++ Module8/SetTimeout.js
@@ -0,0 +1,12 @@
+console.log('Start')
+
+
+
+function greet(){
+    console.log("Swagat nahi karoge humara")
+}
+setTimeout(greet,2000) // setTimeout is an function allows to run after a certain amount of time 
+
+
+
+console.log('End')
