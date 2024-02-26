12799ca3b12f23688c2d5e1f5beaa022161e3405
Arman Salmani <armansalmani596@gmail.com>
2024-02-26 15:58:28 +0530

    Higher Order Function in JS

--- Module6/HOF.js
+++ Module6/HOF.js
@@ -24,6 +24,7 @@ console.log(squaredarr)
 
 
 //A “higher-order function” is a function that accepts functions as parameters and/or returns a function.
+
 //Excercise -> Convert the given indian transaction array into $Dollar 
 
 const transaction1 = [1000, 3000, 4000, 2000 ,-4600]
@@ -45,7 +46,7 @@ const transaction = [1000, 3000, 4000, 2000 ,-4600]
 const intoDollar = 80;
 
 rupeeintodoll=transaction.forEach((dollar)=>{
-    console.log((dollar/intoDollar).toFixed(0)) // to fixed is used to get fixed number not a decimal number 
+    console.log((dollar/intoDollar).toFixed(0)) // tofixed is used to get fixed number not a decimal number 
 })
 
 
