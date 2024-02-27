c9f72597826645cfe4b3c896fbdf642b365c23fb
Arman Salmani <armansalmani596@gmail.com>
2024-02-27 23:13:10 +0530

    Typeof in JS

---
+++ Module9/Typeof.js
@@ -0,0 +1,17 @@
+let a = 4;
+console.log(typeof a);
+
+let b = 'Arman'
+console.log(typeof b);
+
+let object1={
+    name: 'Arman',
+    Age : 50
+}
+console.log(typeof object1);
+
+
+let myArray=[1,5,6,4,5]
+console.log(typeof myArray + "Array") // Here it will show object because in JS Arrays are objects 
+
+// To check Array there is an method called Array.isArray
