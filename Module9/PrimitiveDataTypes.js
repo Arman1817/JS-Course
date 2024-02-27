8647d7200acf7235237b6a9efa9ca94b488e1203
Arman Salmani <armansalmani596@gmail.com>
2024-02-27 23:11:29 +0530

    Primitive DataTypes in JS

---
+++ Module9/PrimitiveDataTypes.js
@@ -0,0 +1,12 @@
+firstperson='Arman'
+SecondPerson=firstperson
+console.log(firstperson)
+console.log(SecondPerson) // SecondPerson and FirstPerson values will be same
+
+firstperson='Arman'
+SecondPerson=firstperson
+firstperson='Daniel'
+console.log(firstperson)
+console.log(SecondPerson) // Here the SecondPerson Value will be not be changed  because primitve DT uses Stack memory
+
+
