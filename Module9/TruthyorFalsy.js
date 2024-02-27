4584797a553cd1ff512c072841dfd6291f0c510a
Arman Salmani <armansalmani596@gmail.com>
2024-02-27 23:12:56 +0530

    Truthy or Falsy in Js

---
+++ Module9/TruthyorFalsy.js
@@ -0,0 +1,18 @@
+a=0
+if (a) {
+    console.log(" I'm a true value ");
+} else {
+    console.log(" I'm a False Value ");
+}
+
+//false: The keyword false
+//0: The number zero, also including 0.0, 0x0, etc.
+//-0: The number negative zero, also including -0.0
+//NaN: Not a number
+//undefined: The primitive value undefined
+//null: The keyword null
+
+//true
+//all numbers (except 0 and NaN)
+//all objects (including arrays and functions)
+//all strings (including the empty string)
